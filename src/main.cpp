bb2d576e88f5b06f1ecf71e5b3d7b00463ac4e50
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-01-09 11:55:10 +0100

    Projet de base

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h"
+#include "p6/p6.h"
+#include "vector"
+
+int main()
+{
+    // Run the tests
+    if (doctest::Context{}.run() != 0)
+        return EXIT_FAILURE;
+
+    // Actual application code
+    auto ctx = p6::Context{{.title = "Projet_P6_THIBAUD_Erwan"}};
+    ctx.maximize_window();
+
+    // Declare your infinite update loop.
+    ctx.update = [&]() {
+        // Clear the background with a fading effect
+        ctx.use_stroke = false;
+        ctx.fill       = {0.2f, 0.1f, 0.3f, 0.1f};
+        ctx.rectangle(p6::FullScreen{});
+        // Draw something
+        ctx.fill = {1.f, 0.7f, 0.2f};
+        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.1f});
+    };
+
+    // Should be done last. It starts the infinite loop.
+    ctx.start();
+}
