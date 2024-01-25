2f595985e811bdf25b89cf2d055feb9c24a43662
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-01-25 10:55:51 +0100

    boid first move

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main()
 {
 // Creat first boid
      Boid firstBoid(0.0, 0.0, 2.0, 3.0);
+    double deltaTime = 0.001;
     
 
     // Run the tests
@@ -26,9 +27,12 @@ int main()
         ctx.fill       = {0.2f, 0.1f, 0.3f, 1.0f};
         ctx.rectangle(p6::Center{0.0, 0.0}, p6::Radii{0.3, 0.5}, p6::Rotation{0.0_turn});
         // Draw something
+
+
         ctx.fill = {1.f, 0.7f, 0.2f};
         //ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.1f});
-        ctx.circle(p6::Center{firstBoid.getPosX(), firstBoid.getPosY()}, p6::Radius{0.1f});
+        ctx.circle(p6::Center{firstBoid.getPosX(), firstBoid.getPosY()}, p6::Radius{0.05f});
+        firstBoid.updatePosition(deltaTime); 
         //std::cout << firstBoid.getPosX() << std::endl;
         
     };
