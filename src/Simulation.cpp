67937d105f4ac7646bbee7c19a775b64dd0cf6b9
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-02-01 09:13:41 +0100

    Compile Windows

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,30 +1,23 @@
 #include "Simulation.hpp"
 
-
-
-
 // Définition du constructeur
-Simulation::Simulation() 
-    : name(this->name.c_str()), window_width(this->window_width), window_height(this->window_height), ctx()
+Simulation::Simulation()
+    : name("Projet Boids"), window_width(this->window_width), window_height(this->window_height), ctx()
 {
     // Initialisations supplémentaires si nécessaires
 }
 
+void Simulation::Run()
+{
+    // initalisation des boids
 
-
-void Simulation::Run(){
-    //initalisation des boids 
-    
-    //lancer la boucle infini 
-    Render(); 
+    // lancer la boucle infini
+    Render();
     this->ctx.start();
 }
 
-
-void Simulation::Render(){
-
-
-
+void Simulation::Render()
+{
     // Declare your infinite update loop.
     ctx.update = [&]() {
         // Clear the background with a fading effect
@@ -33,20 +26,14 @@ void Simulation::Render(){
         ctx.rectangle(p6::Center{0.0, 0.0}, p6::Radii{0.3, 0.5}, p6::Rotation{0.0_turn});
         // Draw something
 
-
         ctx.fill = {1.f, 0.7f, 0.2f};
         ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.1f});
-        //ctx.circle(p6::Center{firstBoid.getPosX(), firstBoid.getPosY()}, p6::Radius{0.05f});
-        //firstBoid.updatePosition(deltaTime); 
-        //std::cout << firstBoid.getPosX() << std::endl;
-        
+        // ctx.circle(p6::Center{firstBoid.getPosX(), firstBoid.getPosY()}, p6::Radius{0.05f});
+        // firstBoid.updatePosition(deltaTime);
+        // std::cout << firstBoid.getPosX() << std::endl;
     };
-
-    
 }
-
 
 // Simulation::~Simulation(){
 
 // }
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,16 @@
 #include <cstdlib>
-#include "vector"
 #include "Boid.hpp"
-#include "p6/p6.h"
-
 #include "Simulation.hpp"
+#include "p6/p6.h"
+#include "vector"
 
 int main()
 {
-// Creat first boid
-     //Boid firstBoid(0.0, 0.0, 2.0, 3.0);
-    //double deltaTime = 0.001;
+    // Creat first boid
+    // Boid firstBoid(0.0, 0.0, 2.0, 3.0);
+    // double deltaTime = 0.001;
 
     Simulation Sim;
     Sim.Run();
-    
-    /*
-
-
-    // Actual application code
-    auto ctx = p6::Context{{.title = "Projet_BOIDS"}};
-    ctx.maximize_window();
-
-    // Declare your infinite update loop.
-    ctx.update = [&]() {
-        // Clear the background with a fading effect
-        ctx.use_stroke = false;
-        ctx.fill       = {0.2f, 0.1f, 0.3f, 1.0f};
-        ctx.rectangle(p6::Center{0.0, 0.0}, p6::Radii{0.3, 0.5}, p6::Rotation{0.0_turn});
-        // Draw something
-
-
-        ctx.fill = {1.f, 0.7f, 0.2f};
-        //ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.1f});
-        ctx.circle(p6::Center{firstBoid.getPosX(), firstBoid.getPosY()}, p6::Radius{0.05f});
-        firstBoid.updatePosition(deltaTime); 
-        //std::cout << firstBoid.getPosX() << std::endl;
-        
-    };
-
-    // Should be done last. It starts the infinite loop.
-    ctx.start();
-
-    */
+    return 0;
 }
