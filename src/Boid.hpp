b8e6fa78bb6bdd845b61c749153f32a0d5f31788
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-02-03 06:28:51 +0100

    Rien de spécial

--- src/Boid.hpp
+++ src/Boid.hpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <iostream>
 
-
 class Boid {
 private:
     float posX;
@@ -13,19 +12,44 @@ private:
     double velocityY;
 
 public:
-    //Constructeur
+    // Constructeur
     Boid(float initPosX, float initPosY, float initVelocityX, float initVelocityY);
 
+    float getPosX() const;
+    float getPosY() const;
 
-    float getPosX() const; 
-    float getPosY() const; 
-
-    void updatePosition(double deltaTime);
+    void  updatePosition(double deltaTime);
     float getSpeed() const;
     float getDirection() const;
-    void printPosition() const;
-    
+    void  printPosition() const;
 
     // Destructeur
     ~Boid();
 };
+
+/*
+
+class Boid {
+public:
+    bool predator;
+    Pvector location;
+    Pvector velocity;
+    Pvector acceleration;
+    float maxSpeed;
+    float maxForce;
+    Boid() {}
+    Boid(float x, float y);
+    Boid(float x, float y, bool predCheck);
+    void applyForce(const Pvector& force);
+    // Three Laws that boids follow
+    Pvector Separation(const vector<Boid>& Boids);
+    Pvector Alignment(const vector<Boid>& Boids);
+    Pvector Cohesion(const vector<Boid>& Boids);
+    //Functions involving SFML and visualisation linking
+    Pvector seek(const Pvector& v);
+    void run(const vector<Boid>& v);
+    void update();
+    void flock(const vector<Boid>& v);
+    void borders();
+    float angle(const Pvector& v);
+};*/
