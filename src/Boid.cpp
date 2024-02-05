e72812e7ce2635ff21f2dc032f8b2980741e998a
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-02-05 15:19:43 +0100

    Merge triangle et clean code

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -3,8 +3,6 @@
 Boid::Boid(float initPosX, float initPosY, float initVelocityX, float initVelocityY)
     : posX(initPosX), posY(initPosY), velocityX(initVelocityX), velocityY(initVelocityY) {}
 
-
-
 float Boid::getPosX() const
 {
     return this->posX;
@@ -14,11 +12,6 @@ float Boid::getPosY() const
     return this->posY;
 }
 
-
-
-
-
-
 float Boid::getVelocityX() const
 {
     return this->velocityX;
@@ -27,14 +20,6 @@ float Boid::getVelocityY() const
 {
     return this->velocityY;
 }
-
-
-
-
-
-
-
-
 
 void Boid::updatePosition(double deltaTime)
 {
@@ -74,10 +59,38 @@ p6::Radians Boid::getDirection() const
     return p6::Radians(std::atan2(velocityY, velocityX));
 }
 
-void Boid::printPosition() const
+void Boid::flock(std::vector<Boid*> Boids)
 {
     const float alignmentFactor = 0.005; // Ajustez cette valeur selon vos besoins
     const float minSpeed        = 0.002; // Ajustez cette valeur selon vos besoins
+
+    /* cohesion factor */
+    const float cohesionFactor = 0.0002; // Ajustez cette valeur selon vos besoins
+
+    const float separationsFactor = 0.00005; // Ajustez cette valeur selon vos besoins
+
+    std::pair<float, float> alignment   = align(Boids);
+    std::pair<float, float> cohesions   = cohesion(Boids);
+    std::pair<float, float> separations = separation(Boids);
+
+    // Appliquer l'alignement comme une fraction de la moyenne des vélocités des voisins
+
+    accelerationX = (alignment.first * alignmentFactor + cohesions.first * cohesionFactor + separations.first * separationsFactor);
+    accelerationY = (alignment.second * alignmentFactor + cohesions.second * cohesionFactor + separations.second * separationsFactor);
+
+    /*
+    accelerationX = separations.first * separationsFactor;
+    accelerationY = separations.second * separationsFactor;
+    */
+    // Limiter la vitesse à minSpeed
+    float speed = std::sqrt(velocityX * velocityX + velocityY * velocityY);
+    if (speed < minSpeed)
+    {
+        float speedScale = minSpeed / speed;
+        velocityX *= speedScale;
+        velocityY *= speedScale;
+    }
+}
 
 void Boid::show(p6::Context& ctx) const
 {
@@ -87,7 +100,6 @@ void Boid::show(p6::Context& ctx) const
     ctx.use_fill      = false;
     ctx.equilateral_triangle(p6::Center{getPosX(), getPosY()}, p6::Radius{0.03f}, p6::Rotation{getDirection()});
 }
-
 
 std::pair<float, float> Boid::align(std::vector<Boid*> Boids)
 {
@@ -216,32 +228,14 @@ std::pair<float, float> Boid::separation(std::vector<Boid*> Boids)
     return std::make_pair(avgVelocityX, avgVelocityY);
 }
 
-
-
-
-
-
-
-
 float Boid::getSpeed() const
 {
     return std::sqrt(velocityX * velocityX + velocityY * velocityY);
-}
-
-float Boid::getDirection() const
-{
-    return std::atan2(velocityY, velocityX);
 }
 
 void Boid::printPosition() const
 {
     std::cout << "posX = " << posX << " | posY = " << posY << std::endl;
 }
-
-
-
-
-
-
 
 Boid::~Boid() {}
--- src/Simulation.hpp
+++ src/Simulation.hpp
@@ -7,12 +7,12 @@
 
 class Simulation {
 private:
-    std::string name          = "Projet_BOIDS";
-    const int   window_width  = 1280;
-    const int   window_height = 720;
-    const unsigned int nb_flock = 100; 
-    const float speed_factor = 0.015; 
-    p6::Context ctx;
+    std::string        name          = "Projet_BOIDS";
+    const int          window_width  = 1280;
+    const int          window_height = 720;
+    const unsigned int nb_flock      = 100;
+    const float        speed_factor  = 0.005;
+    p6::Context        ctx;
 
     std::vector<Boid*> flock;
 
