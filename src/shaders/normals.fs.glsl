cb4db21a6a28dde1ce78c824ff42f861c33729bb
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-03-20 15:18:32 +0100

    git ignore

--- src/shaders/normals.fs.glsl
+++ src/shaders/normals.fs.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 
 in vec3 fragNormal;
-uniform sampler2D uTexture;
+//uniform sampler2D uTexture;
 in vec2 vfragTextureCoord;
 out vec4 fragColor;
 
 void main() {
-    vec4 tex = texture(uTexture, vfragTextureCoord);
+    //vec4 tex = texture(uTexture, vfragTextureCoord);
     // Use normal as color for demonstration purposes
     vec3 normalizedNormal = normalize(fragNormal);
     vec3 color = 1. * normalizedNormal;
